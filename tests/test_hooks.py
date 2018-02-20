b8c49df41171e58bfd48f40b291637346c0ecbaf
Juliano Siloto Assine <jsiloto@cpqd.com.br>
2018-02-20 17:41:57 -0300

    Run Dredd inside Docker

--- tests/test_hooks.py
+++ tests/test_hooks.py
@@ -1,10 +1,6 @@
 import dredd_hooks as hooks
 import jwt
-import time
-import datetime
-
-import subprocess
-from subprocess import CalledProcessError
+import db_fixture
 
 service = 'admin'
 encode_data = {'userid': 1, 'name': 'Admin (superuser)', 'groups': [1], 'iat': 1517339633, 'exp': 1517340053,
@@ -17,20 +13,9 @@ jwt_token = str(encoded, 'ascii')
 
 @hooks.before_each
 def add_api_key(transaction):
-    # Run DB fixture
-    try:
-        proc = subprocess.run(['docker', 'run', '--rm', '--network', 'imgm_default', 'local/db_fixture:latest'],
-                              check=True)
-    except CalledProcessError as err:
-        print(err.stdout)
-        raise
+    db_fixture.run()
 
     # Substitute Authorization with actual token
     auth = 'Bearer ' + jwt_token
     if 'Authorization' in transaction['request']['headers']:
         transaction['request']['headers']['Authorization'] = auth
-
-
-@hooks.after_all
-def my_after_all_hook(transactions):
-    print('after_all')
