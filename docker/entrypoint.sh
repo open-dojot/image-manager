f86e8a89ffe848368889f9f8a0f40935fc666b67
Giovanni Curiel dos Santos <giovannicuriel@gmail.com>
2019-03-13 14:12:06 -0300

    fix: fixing variable increment in entrypoint script

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -29,7 +29,7 @@ if [ $1 = 'start' ]; then
         else
             echo "Cannot start application, retying in $sleep_time seconds..."
             sleep $sleep_time
-            let retries++
+            retries=$((retries+1))
         fi
     done
 fi
